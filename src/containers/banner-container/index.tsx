1c4efcde46099c509445b5306ed10e3344c872a4
Muhammad Shahbaz Mancho <shahbazmancho@gmail.com>
2022-04-15 00:12:10 +0400

    fix: name

--- src/containers/banner-container/index.tsx
+++ src/containers/banner-container/index.tsx
@@ -6,9 +6,11 @@ import { useNavigate } from "react-router-dom";
 export default function BannerContainer() {
   const navigate = useNavigate();
   const { movie } = useGetRandomMovie();
+  const { name, title, original_title } = movie || {};
+  const movieName = title || original_title || name || "";
   return (
     <Banner
-      title={movie?.title ?? ""}
+      title={movieName}
       description={movie?.overview ?? ""}
       imageUrl={`${imageBaseUrlLarge}${movie?.backdrop_path}`}
       onClickMyList={() => {
