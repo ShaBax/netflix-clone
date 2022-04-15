ceae1a126ea3fa0b2aff5ecdc5e4b7add1b592e8
Muhammad Shahbaz Mancho <shahbazmancho@gmail.com>
2022-04-15 10:56:44 +0400

    fix: netlify routes

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,5 @@
 //TODO: move this to env
-export const apiKey = "181beb5f6a96ef3b3e127780a9f51a0b";
+export const apiKey = process.env.API_KEY || "181beb5f6a96ef3b3e127780a9f51a0b";
 export const imageBaseUrl = "https://image.tmdb.org/t/p/original";
 export const imageBaseUrlSmall = "https://image.tmdb.org/t/p/w500";
 export const imageBaseUrlMedium = "https://image.tmdb.org/t/p/w780";
