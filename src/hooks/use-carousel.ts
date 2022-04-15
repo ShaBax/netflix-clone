1bc2b397268c6fa35e44e472c4c8822ac483e0c5
Muhammad Shahbaz Mancho <shahbazmancho@gmail.com>
2022-04-15 12:22:13 +0400

    fix: fixing debouce eslint issue

--- src/hooks/use-carousel.ts
+++ src/hooks/use-carousel.ts
@@ -7,17 +7,15 @@ import { IMovie } from "../apis/type";
 export const useCarousel = () => {
   const dispatch = useAppDispatch();
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleOnAddToList = useCallback(
-    debounce(
-      (data: IMovie) => {
-        dispatch(updateMyList(data));
-      },
-      500,
-      { leading: true, trailing: false, maxWait: 500 }
-    ),
-    []
-  );
+  const addToList = (data: IMovie) => {
+    dispatch(updateMyList(data));
+  };
+  const debounceAddToList = debounce(addToList, 500, {
+    leading: true,
+    trailing: false,
+    maxWait: 500,
+  });
+  const handleOnAddToList = useCallback(debounceAddToList, [debounceAddToList]);
 
   return { handleOnAddToList };
 };
